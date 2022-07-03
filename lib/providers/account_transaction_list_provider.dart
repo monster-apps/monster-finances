d19a568865276991485f2525fafb2d155a69d2ed
Luan Pablo <luanpab@gmail.com>
2022-07-03 13:24:45 -0400

    Refresh total amounts in overview when adding/updating transaction

--- lib/providers/account_transaction_list_provider.dart
+++ lib/providers/account_transaction_list_provider.dart
@@ -1,6 +1,9 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:monster_finances/data/database/entities/transaction.dart';
 import 'package:monster_finances/providers/current_account_provider.dart';
+import 'package:monster_finances/providers/total_amount_by_account_provider.dart';
+import 'package:monster_finances/providers/total_amount_by_account_type_provider.dart';
+import 'package:monster_finances/providers/total_amount_provider.dart';
 import 'package:monster_finances/providers/transaction_query_provider.dart';
 
 final FutureProvider<List<Transaction>> accountTransactionListProvider =
@@ -24,6 +27,12 @@ class AccountTransactionListNotifier extends StateNotifier<List<Transaction>> {
 
   void add(ref, Transaction transaction) async {
     ref.read(transactionQueryProvider).put(transaction);
+
+    ref.refresh(totalAmountProvider);
+    ref.refresh(totalAmountByAccountTypeProvider(
+        transaction.account.target!.type.targetId));
+    ref.refresh(totalAmountByAccountProvider(transaction.account.targetId));
+
     state = ref
         .read(transactionQueryProvider)
         .getAccountTransactions(ref.watch(currentAccountProvider));
